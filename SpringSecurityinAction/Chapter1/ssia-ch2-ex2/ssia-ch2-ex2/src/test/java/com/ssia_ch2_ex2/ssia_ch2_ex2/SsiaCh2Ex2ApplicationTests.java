f042a7e655aba2af00fdab34149894226886acee
luisab96 <336luizitholaab@gmail.com>
2025-02-07 13:19:21 -0600

    Adding Example 2, as fully implemented with authentication filter and security filter chain

---
+++ SpringSecurityinAction/Chapter1/ssia-ch2-ex2/ssia-ch2-ex2/src/test/java/com/ssia_ch2_ex2/ssia_ch2_ex2/SsiaCh2Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssia_ch2_ex2.ssia_ch2_ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh2Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
