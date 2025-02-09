8e31bee7a9a518e75a2f9acf246d13f9997491c3
luisab96 <336luizitholaab@gmail.com>
2025-02-09 13:53:30 -0600

    Split Authentication provider with userMgt and WebAuthorizationConfig

---
+++ SpringBUpandRunning/e1/src/main/java/com/UpandRunning/e1/E1Application.java
@@ -0,0 +1,14 @@
+package com.UpandRunning.e1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.UUID;
+
+@SpringBootApplication
+public class E1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E1Application.class, args);
+	}
+}
